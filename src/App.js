81301758e5072fe2a9ccf11f47096dcbea4727bc
Avinash Tiwari <aviprf10@gmail.com>
2023-12-23 21:59:41 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [fullName, setFullName] = useState('');
+
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value);
+  };
+
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevents the form from submitting in the traditional way
+
+    if (firstName && lastName) {
+      const fullName = `${firstName} ${lastName}`;
+      setFullName(fullName);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label>
+          First Name:
+          <input type="text" value={firstName} onChange={handleFirstNameChange} />
+        </label>
+        <br />
+        <label>
+          Last Name:
+          <input type="text" value={lastName} onChange={handleLastNameChange} />
+        </label>
+        <br />
+        <button type="submit" disabled={!firstName || !lastName}>
+          Submit
+        </button>
+      </form>
+      {fullName && <p>Full Name: {fullName}</p>}
     </div>
   );
 }
-
-export default App;
